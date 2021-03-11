f18a4a70a8c35ae248d11f61c964fa248aa51856
s2k <>
2021-03-10 21:12:44 -0800

    detecting windows version and architecture

--- THUD_Unblock.py
+++ THUD_Unblock.py
@@ -1,6 +1,7 @@
 import ctypes
 import os
 import pathlib
+import platform
 import sys
 import time
 import win32api
@@ -9,14 +10,40 @@ import wmi
 from injector import Injector
 from ctypes import *
 
-# Path to inject file
-dll_path = os.path.join(pathlib.Path(__file__).parent.absolute(),'win7x64_dll_inject.dll')
+ 
+
+
 
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
     except:
         return False
+
+def determine_dll_file():
+    if platform.platform().lower().startswith('windows-7'):
+        win_platform = 'win7'
+    elif platform.platform().lower().startswith('windows-10'):
+        win_platform = 'win10'
+    else:
+        print('Could not find Windows version')
+        sys.exit(1)
+
+    if platform.architecture()[0].startswith('64'):
+        win_arch = 64
+    elif platform.architecture()[0].startswith('32'):
+        win_arch = 32
+    else:
+        print('Could not find the Windows Architecture')
+        sys.exit(1)
+
+    if win_platform == 'win7' and win_arch == 64:
+        return os.path.join(pathlib.Path(__file__).parent.absolute(),'win7x64_dll_inject.dll')  
+    if win_platform == 'win10' and win_arch == 64:
+        return os.path.join(pathlib.Path(__file__).parent.absolute(),'win10x64_dll_inject.dll')
+
+# Path to inject file
+dll_path = determine_dll_file()
 
 class THUD_Unblock(object):
     
@@ -43,19 +70,19 @@ class THUD_Unblock(object):
             print('No PID')
             sys.exit(1)
             
-        print("Full DLL path: %s" %dll_path)
+        print('Full DLL path: %s' %dll_path)
 
         # Inject DLL into THUD
-        print("Injecting...")
+        print('Injecting...')
         injector = Injector()
         injector.load_from_pid(pid)
         injector.inject_dll(dll_path)
         injector.unload()
-        print("Injectine Done.")
+        print('Injectine Done.')
 
         # Countdown until exit
         for i in range(10,1,-1):
-            print("Exiting in ... %s" %i)
+            print('Exiting in ... %s' %i)
             time.sleep(1)
 
 def main():
